f0ed0cc225e3accf389cc08ba272b2ed24631e46
Priyanka Takale <85220239+Priyanka2101-02@users.noreply.github.com>
2023-08-10 21:13:33 +0530

    Add files via upload

---
+++ Encapsulation/Hardware.java
@@ -0,0 +1,55 @@
+package oops;
+
+public class Hardware {
+	private String desc;
+	private String name;
+	private int qty;
+	private double price;
+
+	public String getDesc() {
+		return desc;
+	}
+
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getQty() {
+		return qty;
+	}
+
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
